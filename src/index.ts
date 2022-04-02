562368f44a445c4cf91b412bc3788ec2338e7d9a
James Croall <jcroall@pobox.com>
2022-04-02 20:23:12 +0000

    Final flow

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,12 @@ import {
   gitlabGetDiscussions,
   gitlabGetProject,
   gitlabUpdateNote,
-  CoverityIssuesView,
-  CoverityProjectIssue,
-  coverityMapMatchingMergeKeys,
-  coverityCreateReviewCommentMessage,
-  coverityCreateIssueCommentMessage,
-  COVERITY_COMMENT_PREFACE,
-  coverityIsInDiff,
   coverityIsPresent,
-  coverityCreateNoLongerPresentMessage, CoverityIssueOccurrence
+  coverityCreateNoLongerPresentMessage
 } from "@jcroall/synopsys-sig-node/lib/"
 
 import {logger} from "@jcroall/synopsys-sig-node/lib";
 import * as fs from "fs";
-import {relatavize_path} from "@jcroall/synopsys-sig-node/lib/paths";
 import {gitlabCreateDiscussionWithoutPosition} from "@jcroall/synopsys-sig-node/lib/gitlab/discussions";
 import {PolarisTaskInputs} from "@jcroall/synopsys-sig-node/lib/polaris/model/PolarisTaskInput";
 import PolarisConnection from "@jcroall/synopsys-sig-node/lib/polaris/model/PolarisConnection";
@@ -34,6 +26,12 @@ import PolarisInstall from "@jcroall/synopsys-sig-node/lib/polaris/model/Polaris
 import PolarisRunner from "@jcroall/synopsys-sig-node/lib/polaris/cli/PolarisRunner";
 import * as os from "os";
 import PolarisIssueWaiter from "@jcroall/synopsys-sig-node/lib/polaris/util/PolarisIssueWaiter";
+import {
+  POLARIS_COMMENT_PREFACE, polarisCreateReviewCommentMessage,
+  polarisGetIssuesUnified,
+  polarisGetRuns, polarisIsInDiff
+} from "@jcroall/synopsys-sig-node/lib/polaris/service/PolarisAPI";
+import {IPolarisIssueUnified} from "@jcroall/synopsys-sig-node/lib/polaris/model/PolarisAPI";
 
 const chalk = require('chalk')
 const figlet = require('figlet')
@@ -50,8 +48,9 @@ export async function main(): Promise<void> {
   program
       .description("Integrate Synopsys Polaris Static Analysis into GitLab")
       .option('-u, --polaris-url <Polaris URL>', 'Location of the Polaris service')
-      .option('-a, --polaris-args <Polaris arguments>', 'Arguments to pass to polaris CLI')
-      .option('-b, --build-command <Build Command>', 'Command for building the project from source code')
+      .option('-s, --skip-run', 'Skip running Polaris, assume it is already complete')
+      .option('-g, --gitlab-security', 'Enable GitLab security dashboard')
+      .option('-c, --polaris-command <Build Command>', 'Command line to pass to polaris CLI')
       .option('-d, --debug', 'Enable debug mode (extra verbosity)')
       .parse(process.argv)
 
@@ -66,12 +65,13 @@ export async function main(): Promise<void> {
   const POLARIS_PROXY_USERNAME = process.env['POLARIS_PROXY_USERNAME']
   const POLARIS_PROXY_PASSWORD = process.env['POLARIS_PROXY_PASSWORD']
 
-  logger.info(`url=${options.polarisUrl}`)
   let polaris_url = options.polarisUrl ? options.polarisUrl as string : POLARIS_URL
   if (!polaris_url) {
     logger.error(`Must specify Polaris URL in arguments or environment`)
     process.exit(1)
   }
+
+  let skipRun = options.skipRun
 
   if (!POLARIS_ACCESS_TOKEN) {
     logger.error(`Mist specify Polaris Access Token in POLARIS_ACCESS_TOKEN`)
@@ -87,8 +87,7 @@ export async function main(): Promise<void> {
     logger.debug(`Enabled debug mode`)
   }
 
-  let build_command = options.buildCommand ? options.buildCommand : ""
-  let polaris_args = options.polarisArgs ? options.polarisArgs : ""
+  let polaris_command = options.polarisCommand ? options.polarisCommand : ""
 
   const GITLAB_TOKEN = process.env['GITLAB_TOKEN']
   const CI_SERVER_URL = process.env['CI_SERVER_URL']
@@ -162,146 +161,125 @@ export async function main(): Promise<void> {
 
   const merge_request_iid = parseInt(CI_MERGE_REQUEST_IID, 10)
 
+  let polaris_run_result = undefined
 
-  //If there are no changes, we can potentially bail early, so we do that first.
-  var actual_build_command = build_command;
-  if (merge_request_iid > 0 && task_input.should_populate_changeset) {
-    logger.debug("Populating change set for Polaris Software Integrity Platform.");
-    const changed_files = await gitlabGetChangesForMR(CI_SERVER_URL, GITLAB_TOKEN, CI_PROJECT_ID, merge_request_iid)
-    if (changed_files.length == 0 && task_input.should_empty_changeset_fail) {
-      logger.error(` Task failed: No changed files were found.`)
-      return;
-    } else if (changed_files.length == 0) {
-      logger.info("Task finished: No changed files were found.")
-      return;
-    }
-    const change_set_environment = new ChangeSetEnvironment(logger, process.env);
-    const change_file = change_set_environment.get_or_create_file_path(process.cwd());
-    change_set_environment.set_enable_incremental();
-
-    await new ChangeSetFileWriter(logger).write_change_set_file(change_file, changed_files);
-    actual_build_command = new ChangeSetReplacement().replace_build_command(actual_build_command, change_file);
-
-    logger.debug(`actual_build_command=${actual_build_command} change_file=${change_file}`)
-  }
-
-  logger.info("Installing Polaris Software Integrity Platform.");
-  var polaris_installer = PolarisInstaller.default_installer(logger, polaris_service);
-  var polaris_install: PolarisInstall = await polaris_installer.install_or_locate_polaris(connection.url, polaris_install_path);
-  logger.info("Found Polaris Software Integrity Platform: " + polaris_install.polaris_executable);
-
-  logger.info("Running Polaris Software Integrity Platform.");
-  var polaris_runner = new PolarisRunner(logger);
-  var polaris_run_result = await polaris_runner.execute_cli(connection, polaris_install, process.cwd(), actual_build_command);
-
-  if (task_input.should_wait_for_issues) {
-    logger.info("Checking for issues.")
-    var polaris_waiter = new PolarisIssueWaiter(logger);
-    var issue_count = await polaris_waiter.wait_for_issues(polaris_run_result.scan_cli_json_path, polaris_service);
-    if (issue_count != null && issue_count > 0) {
-      logger.error(`Polaris Software Integrity Platform found ${issue_count} total issues.`)
+  if (skipRun) {
+    polaris_run_result = {
+      scan_cli_json_path: ".synopsys/polaris/cli-scan.json",
+      return_code: 0
     }
   } else {
-    logger.info("Will not check for issues.")
+    //If there are no changes, we can potentially bail early, so we do that first.
+    var actual_build_command = polaris_command;
+    if (merge_request_iid > 0 && task_input.should_populate_changeset) {
+      logger.debug("Populating change set for Polaris Software Integrity Platform.");
+      const changed_files = await gitlabGetChangesForMR(CI_SERVER_URL, GITLAB_TOKEN, CI_PROJECT_ID, merge_request_iid)
+      if (changed_files.length == 0 && task_input.should_empty_changeset_fail) {
+        logger.error(` Task failed: No changed files were found.`)
+        return;
+      } else if (changed_files.length == 0) {
+        logger.info("Task finished: No changed files were found.")
+        return;
+      }
+      const change_set_environment = new ChangeSetEnvironment(logger, process.env);
+      const change_file = change_set_environment.get_or_create_file_path(process.cwd());
+      change_set_environment.set_enable_incremental();
+
+      await new ChangeSetFileWriter(logger).write_change_set_file(change_file, changed_files);
+      actual_build_command = new ChangeSetReplacement().replace_build_command(actual_build_command, change_file);
+    }
+
+    logger.info("Installing Polaris Software Integrity Platform.");
+    var polaris_installer = PolarisInstaller.default_installer(logger, polaris_service);
+    var polaris_install: PolarisInstall = await polaris_installer.install_or_locate_polaris(connection.url, polaris_install_path);
+    logger.info("Found Polaris Software Integrity Platform: " + polaris_install.polaris_executable);
+
+    logger.info("Running Polaris Software Integrity Platform.");
+    var polaris_runner = new PolarisRunner(logger);
+    polaris_run_result = await polaris_runner.execute_cli(connection, polaris_install, process.cwd(), actual_build_command);
+
+    if (task_input.should_wait_for_issues) {
+      logger.info("Checking for issues.")
+      var polaris_waiter = new PolarisIssueWaiter(logger);
+      var issue_count = await polaris_waiter.wait_for_issues(polaris_run_result.scan_cli_json_path, polaris_service);
+      logger.error(`Polaris Software Integrity Platform found ${issue_count} total issues.`)
+    } else {
+      logger.info("Will not check for issues.")
+    }
+
   }
 
-  logger.info("Task completed.")
+  if (!polaris_run_result) {
+    logger.error(`Unable to find Polaris run results.`)
+    process.exit(2)
+  }
+
+  var scan_json_text = fs.readFileSync(polaris_run_result.scan_cli_json_path);
+  var scan_json = JSON.parse(scan_json_text.toString());
+
+  const json_path = require('jsonpath');
+  var project_id = json_path.query(scan_json, "$.projectInfo.projectId")
+  var branch_id = json_path.query(scan_json, "$.projectInfo.branchId")
+  var revision_id = json_path.query(scan_json, "$.projectInfo.revisionId")
+
+  logger.debug(`Connect to Polaris: ${polaris_service.polaris_url} and fetch issues for project: ${project_id} and branch: ${branch_id}`)
+
+  let runs = await polarisGetRuns(polaris_service, project_id, branch_id)
+
+  if (runs.length > 1) {
+    logger.debug(`Most recent run is: ${runs[0].id} was created on ${runs[0].attributes["creation-date"]}`)
+    logger.debug(`Last run is: ${runs[1].id} was created on ${runs[1].attributes["creation-date"]}`)
+    logger.debug(`...`)
+  }
+
+  let issuesUnified = await polarisGetIssuesUnified(polaris_service, project_id, branch_id, false, runs[0].id, "earlier")
 
   logger.info("Executed Polaris Software Integrity Platform: " + polaris_run_result.return_code);
 
-  /*
-  // Collect all Coverity data and generate optional GitLab Security dashboard before interacting with GitLab
-
-  // TODO validate file exists and is .json?
-  const jsonV7Content = fs.readFileSync(coverity_results_file)
-  const coverityIssues = JSON.parse(jsonV7Content.toString()) as CoverityIssuesView
-
-  let mergeKeyToIssue = new Map<string, CoverityProjectIssue>()
-
-  const canCheckCoverity = coverity_url && COV_USER && COVERITY_PASSPHRASE && coverity_project_name
-  if (!canCheckCoverity) {
-    logger.warn('Missing Coverity Connect info. Issues will not be checked against the server.')
-  } else {
-    const allMergeKeys = coverityIssues.issues.map(issue => issue.mergeKey)
-    const allUniqueMergeKeys = new Set<string>(allMergeKeys)
-
-    if (canCheckCoverity && coverityIssues && coverityIssues.issues.length > 0) {
-      try {
-        mergeKeyToIssue = await coverityMapMatchingMergeKeys(coverity_url, COV_USER, COVERITY_PASSPHRASE,
-            coverity_project_name, allUniqueMergeKeys)
-      } catch (error: any) {
-        logger.error(error as string | Error)
-        process.exit(1)
-      }
-    }
-  }
-
   if (options.gitlabSecurity) {
     logger.info(`Generating GitLab Security Dashboard output: ${GITLAB_SECURITY_DASHBOARD_SAST_FILE}`)
-    let gitlab_json = gitlab_initialize_coverity_json()
-    for (const issue of coverityIssues.issues) {
-      const projectIssue = mergeKeyToIssue.get(issue.mergeKey)
-      let cid_url = undefined
-      if (projectIssue) {
-        cid_url = `${COVERITY_URL}/query/defects.htm?project=${COVERITY_PROJECT}&cid=${projectIssue.cid}`
-      } else {
-        cid_url = ''
-      }
-
-      gitlab_json.vulnerabilities.push(gitlab_get_coverity_json_vulnerability(issue, cid_url))
+    let gitlab_json = gitlab_initialize_polaris_json()
+    for (const issue of issuesUnified) {
+      gitlab_json.vulnerabilities.push(gitlab_get_polaris_json_vulnerability(issue))
     }
 
     fs.writeFileSync(GITLAB_SECURITY_DASHBOARD_SAST_FILE, JSON.stringify(gitlab_json, null, 2), 'utf8')
   }
+
 
   if (!is_merge_request) {
     logger.info('Not a Pull Request, nothing else to do.')
     return
   }
 
-  const merge_request_iid = parseInt(CI_MERGE_REQUEST_IID, 10)
-
   logger.info(`Connecting to GitLab: ${CI_SERVER_URL}`)
 
   let project = await gitlabGetProject(CI_SERVER_URL, GITLAB_TOKEN, CI_PROJECT_ID)
-  logger.debug(`Project=${project.name}`)
-
-
 
   const review_discussions = await gitlabGetDiscussions(CI_SERVER_URL, GITLAB_TOKEN, CI_PROJECT_ID, merge_request_iid).
-    then(discussions => discussions.filter(discussion => discussion.notes![0].body.includes(COVERITY_COMMENT_PREFACE)))
+    then(discussions => discussions.filter(discussion => discussion.notes![0].body.includes(POLARIS_COMMENT_PREFACE)))
   const diff_map = await gitlabGetDiffMap(CI_SERVER_URL, GITLAB_TOKEN, CI_PROJECT_ID, merge_request_iid)
 
-  for (const issue of coverityIssues.issues) {
-    logger.info(`Found Coverity Issue ${issue.mergeKey} at ${issue.strippedMainEventFilePathname}:${issue.mainEventLineNumber}`)
+  for (const issue of issuesUnified) {
+    logger.info(`Found Polaris Issue ${issue.key} at ${issue.path}:${issue.line}`)
 
-    const projectIssue = mergeKeyToIssue.get(issue.mergeKey)
-    let ignoredOnServer = false
-    let newOnServer = true
-    if (projectIssue) {
-      ignoredOnServer = projectIssue.action == 'Ignore' || projectIssue.classification in ['False Positive', 'Intentional']
-      newOnServer = projectIssue.firstSnapshotId == projectIssue.lastSnapshotId
-      logger.info(`Issue state on server: ignored=${ignoredOnServer}, new=${newOnServer}`)
-    }
+    logger.info(`Issue state on server: ${issue.dismissed ? "Dismissed" : "Not Dismissed"}`)
 
-    const reviewCommentBody = coverityCreateReviewCommentMessage(issue)
+    const reviewCommentBody = polarisCreateReviewCommentMessage(issue)
 
-    let path = issue.strippedMainEventFilePathname.startsWith('/') ?
-        relatavize_path(process.cwd(), issue.strippedMainEventFilePathname) :
-        issue.strippedMainEventFilePathname
+    let path = issue.path
 
-    let file_link = `${CI_SERVER_URL}/${process.env.CI_PROJECT_NAMESPACE}/${process.env.CI_PROJECT_NAME}/-/blob/${process.env.CI_COMMIT_REF_NAME}/${path}#L${issue.mainEventLineNumber}`
-    const issueCommentBody = coverityCreateIssueCommentMessage(issue, file_link)
+    const issueCommentBody = polarisCreateReviewCommentMessage(issue)
 
     const review_discussion_index = review_discussions.findIndex(
-        discussion => discussion.notes![0].position?.new_line === issue.mainEventLineNumber &&
-            discussion.notes![0].body.includes(issue.mergeKey))
+        discussion => discussion.notes![0].position?.new_line === issue.line &&
+            discussion.notes![0].body.includes(issue.key))
     let existing_discussion = undefined
     if (review_discussion_index !== -1) {
       existing_discussion = review_discussions.splice(review_discussion_index, 1)[0]
     }
 
-    const comment_index = review_discussions.findIndex(discussion => discussion.notes![0].body.includes(issue.mergeKey))
+    const comment_index = review_discussions.findIndex(discussion => discussion.notes![0].body.includes(issue.key))
     let existing_comment = undefined
     if (comment_index !== -1) {
       existing_comment = review_discussions.splice(comment_index, 1)[0]
@@ -328,15 +306,13 @@ export async function main(): Promise<void> {
               logger.error(`Unable to update discussion: ${error.message}`)
         })
       }
-    } else if (ignoredOnServer) {
+    } else if (issue.dismissed) {
       logger.info('Issue ignored on server, no comment needed.')
-    } else if (!newOnServer) {
-      logger.info('Issue already existed on server, no comment needed.')
-    } else if (coverityIsInDiff(issue, diff_map)) {
+    } else if (polarisIsInDiff(issue, diff_map)) {
       logger.info('Issue not reported, adding a comment to the review.')
 
-      await gitlabCreateDiscussion(CI_SERVER_URL, GITLAB_TOKEN, CI_PROJECT_ID, merge_request_iid, issue.mainEventLineNumber,
-          issue.strippedMainEventFilePathname, reviewCommentBody, CI_MERGE_REQUEST_DIFF_BASE_SHA ? CI_MERGE_REQUEST_DIFF_BASE_SHA : '',
+      await gitlabCreateDiscussion(CI_SERVER_URL, GITLAB_TOKEN, CI_PROJECT_ID, merge_request_iid, issue.line,
+          issue.path, reviewCommentBody, CI_MERGE_REQUEST_DIFF_BASE_SHA ? CI_MERGE_REQUEST_DIFF_BASE_SHA : '',
           CI_COMMIT_SHA ? CI_COMMIT_SHA : '').catch(error => {
             logger.error(`Unable to create discussion: ${error.message}`)
       })
@@ -360,73 +336,68 @@ export async function main(): Promise<void> {
     }
   }
 
-  logger.info(`Found ${coverityIssues.issues.length} Coverity issues.`)
+  logger.info(`Found ${issuesUnified.length} Coverity issues.`)
 
-  if (coverityIssues.issues.length > 0) {
+  if (issuesUnified.length > 0) {
     process.exit(1)
   } else {
     process.exit(0)
   }
-
-   */
 }
 
-/*
-function gitlab_get_coverity_json_vulnerability(issue: CoverityIssueOccurrence, cid_url: string) : any {
+function gitlab_get_polaris_json_vulnerability(issue: IPolarisIssueUnified) : any {
   let json_vlun = {
-    id: issue.mergeKey,
-    cve: issue.mergeKey,
+    id: issue.key,
+    cve: issue.key,
     category: "sast",
-    name: issue.checkerProperties?.subcategoryShortDescription,
-    message: issue.checkerProperties?.subcategoryShortDescription,
-    description: issue.checkerProperties?.subcategoryLongDescription,
-    severity: issue.checkerProperties?.impact,
+    name: issue.name,
+    message: issue.name,
+    description: issue.description,
+    severity: issue.severity,
     confidence: "High",
     scanner: {
-      id: "synopsys_coverity",
-      name: "Synopsys Coverity"
+      id: "synopsys_polaris",
+      name: "Synopsys Polaris"
     },
     location: {
-      file: issue.strippedMainEventFilePathname,
-      start_line: issue.mainEventLineNumber,
-      end_line: issue.mainEventLineNumber,
-      class: issue.functionDisplayName ? issue.functionDisplayName : "",
-      method: issue.functionDisplayName ? issue.functionDisplayName : ""
+      file: issue.path,
+      start_line: issue.line,
+      end_line: issue.line,
+      class: "",
+      method:  ""
     },
     identifiers: [
       {
-        type: "synopsys_coverity_type",
-        name: `Coverity ${issue.checkerName}`,
+        type: "synopsys_polaris_type",
+        name: `Polaris ${issue.checkerName}`,
         value: issue.checkerName,
-        url: (cid_url && cid_url.length > 0) ? cid_url : "http://url-not-available-for-this-issue"
+        url: issue.link
       }
     ]
   }
+  json_vlun.identifiers[0].url = issue.link
 
-  if (cid_url && cid_url.length > 0) {
-    json_vlun.identifiers[0].url = cid_url
-  }
-
-  if (issue.checkerProperties?.cweCategory && issue.checkerProperties.cweCategory != "none") {
-    let cwe_identifer = {
-      type: "cwe",
-      name: `CWE-${issue.checkerProperties.cweCategory}`,
-      value: issue.checkerProperties.cweCategory,
-      url: `https://cwe.mitre.org/data/definitions/${issue.checkerProperties.cweCategory}.html`
+  if (issue.cwe != "N/A") {
+    let cwe_values = issue.cwe.split(', ')
+    for (const cwe_value of cwe_values) {
+      let cwe_identifer = {
+        type: "cwe",
+        name: `CWE-${cwe_value}`,
+        value: cwe_value,
+        url: `https://cwe.mitre.org/data/definitions/${cwe_value}.html`
+      }
+      json_vlun.identifiers.push(cwe_identifer)
     }
-    json_vlun.identifiers.push(cwe_identifer)
   }
 
   return(json_vlun)
 }
 
-
-function gitlab_initialize_coverity_json() : any {
+function gitlab_initialize_polaris_json() : any {
   return {
     version: '2.0',
     vulnerabilities: []
   };
 }
- */
 
 main()
